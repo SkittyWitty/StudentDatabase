4d0dfeadc2102bd4bfcc4ab277c8c83345946a03
Mindy DeWaal <kawiititan@gmail.com>
2021-10-04 21:57:53 -0700

    Commenting ordering strategies

--- orderingStrategy.py
+++ orderingStrategy.py
@@ -1,13 +1,16 @@
 
 """
- Defintion
+Contains all possible stratgies that may be used by the Student Database
 """
 
 def orderByGpa(incomingPartition, currentPartition):
+    # Order Database by GPA known the be the second value in a data entry partition
     return incomingPartition.value1 < currentPartition.value1
 
 def orderbyRedID(incomingPartition, currentPartition):
+    # Order Database by Red ID known the be the second value in a data entry partition
     return incomingPartition.value2 < currentPartition.value2
 
 def orderByName(incomingPartition, currentPartition):
+    # Order Database by Name known the be the second value in a data entry partition
     return incomingPartition.key < currentPartition.key
